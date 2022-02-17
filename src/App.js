a0859ee80ad72de47bee404ba5417370570233cf
juheesvt <kangjuhee18@kookmin.ac.kr>
2022-02-18 01:01:01 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import {DndProvider, useDrag, useDrop} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
+
+const MovableItem = ({setIsDataSlot}) => {
+  const [{isDragging}, drag] = useDrag({
+    type: 'data',
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+
+      if (dropResult && dropResult.name === "데이터 슬롯") {
+        setIsDataSlot(true);
+      } else {
+        setIsDataSlot(false);
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const opacity = isDragging ? 0.4 : 1;
+
+  return (
+      <div ref={drag} className={"movable-item"} style={{opacity}}>
+        Data 1
+      </div>
+  )
+}
+
+// @ts-ignore
+const Slot = ({title, children, className, dataType}) => {
+  const [, drop] = useDrop({
+    accept: dataType,
+    drop: () => ({name: {title}}),
+  });
+
+  return (
+      <div ref={drop} className={className}>
+        {title}
+        {children}
+      </div>
+  )
+}
+
 
 function App() {
+
+  const [isDataSlot, setIsDataSlot] = useState(true);
+  const Item = <MovableItem setIsDataSlot={setIsDataSlot}/>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <DndProvider backend={HTML5Backend}>
+          <Slot title={"data list"} className={"column data-list"} dataType={"everybody"}>
+            {isDataSlot && Item}
+          </Slot>
+
+          <Slot title={"데이터 슬롯"} className={"column"} dataType={"data"}>
+            {!isDataSlot && Item}
+          </Slot>
+          <Slot title={"함수 슬롯"} className={"column"} dataType={"function"}>
+            {null}
+          </Slot>
+          <Slot title={"결과 슬롯"} className={"column"} dataType={"data"}>
+            {null}
+          </Slot>
+        </DndProvider>
+      </div>
   );
 }
 
